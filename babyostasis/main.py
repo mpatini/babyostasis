afd804848efbe754a50dcd9eaf411825d8b715b0
Michael <Michael@tests-MacBook-Pro-2.local>
2014-02-15 16:51:03 -0500

    adding twilio to Alert class

--- babyostasis/main.py
+++ babyostasis/main.py
@@ -2,6 +2,7 @@ import os
 import re
 
 import sendgrid
+from twilio.rest import TwilioRestClient
 
 import webapp2
 import jinja2
@@ -31,9 +32,18 @@ class TestPage(MainHandler):
 
 class Alert(MainHandler):
 	def get(self):
+        # SendGrid
 		sg = sendgrid.SendGridClient('mpatini', 'footyy612')
 		message = sendgrid.Mail(to='mpatini@me.com', subject='Baby Alert', html="You're baby is too cold!", text="You're baby is too cold!", from_email='mpatini@sas.upenn.edu')
 		sg.send(message)
+        #Twilio
+        account_sid = "ACaefb3fc1b4e90f423de6e3695886d4a0"
+        auth_token  = "31f62a7a0969c0032095e3a5fe8d0171"
+        client = TwilioRestClient(account_sid, auth_token)
+        message = client.messages.create(body="ALERT: you're baby is too cold!",
+                                         to="19512883162",
+                                         from_="19094522970")
+        print message.sid
 
 
 app = webapp2.WSGIApplication([('/', TestPage),
