03a35a12c33a22c010873ca6afaceeb463de5a68
Michael <Michael@tests-MacBook-Pro-2.local>
2014-02-15 16:41:28 -0500

    reverted to version w/o twillio to test bug

--- babyostasis/main.py
+++ babyostasis/main.py
@@ -37,29 +37,13 @@ class TestPage(MainHandler):
 
 class Alert(MainHandler):
 	def get(self):
-
-        # SendGrid
-    	sg = sendgrid.SendGridClient('mpatini', 'footyy612')
-    	message = sendgrid.Mail(to='mpatini@me.com', subject='Baby Alert', html="You're baby is too cold!", text="You're baby is too cold!", from_email='mpatini@sas.upenn.edu')
-    	sg.send(message)
-
-        # Twilio
-        account_sid = "ACaefb3fc1b4e90f423de6e3695886d4a0"
-        auth_token  = "31f62a7a0969c0032095e3a5fe8d0171"
-        client = TwilioRestClient(account_sid, auth_token)
-
-        message = client.messages.create(body="ALERT: you're baby is too cold!",
-            to="19512883162",
-            from_="19094522970")
-        print message.sid
-
-        # redirect
-        self.redirect("/")
-
+		sg = sendgrid.SendGridClient('mpatini', 'footyy612')
+		message = sendgrid.Mail(to='mpatini@me.com', subject='Baby Alert', html="You're baby is too cold!", text="You're baby is too cold!", from_email='mpatini@sas.upenn.edu')
+		sg.send(message)
 
 
 app = webapp2.WSGIApplication([('/', TestPage),
-							   ('/alert', Alert),
+							                 ('/alert', Alert),
                                ],
                               debug=True)
 
