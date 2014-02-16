91285c68dcff9f1d901d1ff4531081c8321c216a
Michael <Michael@vagvlan504.0439.wlan.wireless-pennnet.upenn.edu>
2014-02-16 11:11:36 -0500

    muhan's number

--- babyostasis/main.py
+++ babyostasis/main.py
@@ -73,8 +73,11 @@ def alert():
     account_sid = "ACaefb3fc1b4e90f423de6e3695886d4a0"
     auth_token  = "31f62a7a0969c0032095e3a5fe8d0171"
     client = TwilioRestClient(account_sid, auth_token)
+    #message = client.messages.create(body="You're baby is uncomfortably warm. Please check on your baby.",
+    #                                 to="19512883162",
+    #                                 from_="19094522970")
     message = client.messages.create(body="You're baby is uncomfortably warm. Please check on your baby.",
-                                     to="19512883162",
+                                     to="16179538381",
                                      from_="19094522970")
     print message.sid
 
