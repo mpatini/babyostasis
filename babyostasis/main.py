c6747aa30c44382af1fb9d9ecaa0df96c1be33ea
Michael <Michael@tests-MacBook-Pro-2.local>
2014-02-15 22:26:14 -0500

    update MainHandler

--- babyostasis/main.py
+++ babyostasis/main.py
@@ -39,7 +39,7 @@ class Alert(MainHandler):
         account_sid = "ACaefb3fc1b4e90f423de6e3695886d4a0"
         auth_token  = "31f62a7a0969c0032095e3a5fe8d0171"
         client = TwilioRestClient(account_sid, auth_token)
-        message = client.messages.create(body="ALERT: you're baby is too cold!",
+        message = client.messages.create(body="ALERT: you're baby is too warm!",
                                          to="19512883162",
                                          from_="19094522970")
         print message.sid
